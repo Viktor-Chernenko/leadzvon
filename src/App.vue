c2de633905862df64f886d4e03d54c772b7dfbbc
Viktor-Chernenko <chernenkoviktor1991@gmail.com>
2021-05-12 20:17:08 +0300

    added a component 'operatorRating'

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,21 @@
           <workingTime />
           <workingTime :strokeShow="true" :strokeWidth="5" donutSize="78%" />
         </v-row>
+        <v-row class="justify-space-around my-10">
+          <operatorRating :users="usersTest1" />
+          <operatorRating
+            :users="usersTest2"
+            title="Топ операторов по % выкупа"
+            backgroundCard="brown lighten-4"
+            backgroundTabs="brown lighten-3"
+            backgroundAvatar="grey lighten-5"
+            borderListItem="rgba(152, 128, 96, 0.4)"
+            BaseColor="brown--text text--darken-1"
+            colorTabActive="#8a7b67"
+            colorAvatarText="brown--text text--lighten-3"
+            ValueRatingColor="purple--text text--accent-3"
+          />
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
@@ -13,16 +28,85 @@
 
 <script>
 import workingTime from "./components/workingTime";
+import operatorRating from "./components/operatorRating";
 
 export default {
   name: "App",
 
   components: {
     workingTime,
+    operatorRating,
   },
 
   data: () => ({
-    test: "test",
+    usersTest1: {
+      1: {
+        avatar: undefined,
+        positionInTheList: 1,
+        amountOfMoney: "$8,478.11",
+        id: 1,
+        name: "Sandro",
+        surname: "Tavartkiladze",
+      },
+      2: {
+        avatar: "ava-user-2.jpg",
+        positionInTheList: 2,
+        amountOfMoney: "$3,900.50",
+        id: 2,
+        name: "Max",
+        surname: "Mustermann",
+      },
+      3: {
+        avatar: "ava-user-3.jpg",
+        positionInTheList: 3,
+        amountOfMoney: "$3,900.50",
+        id: 3,
+        name: "Marina",
+        surname: "Agapova",
+      },
+      158: {
+        avatar: "ava-user-158.jpg",
+        positionInTheList: 158,
+        amountOfMoney: "$3,900.50",
+        id: 158,
+        name: "My",
+        surname: "Position",
+      },
+    },
+    usersTest2: {
+      1: {
+        avatar: undefined,
+        positionInTheList: 1,
+        amountOfMoney: "55%",
+        id: 1,
+        name: "Sandro",
+        surname: "Tavartkiladze",
+      },
+      2: {
+        avatar: "ava-user-2.jpg",
+        positionInTheList: 2,
+        amountOfMoney: "34%",
+        id: 2,
+        name: "Max",
+        surname: "Mustermann",
+      },
+      3: {
+        avatar: "ava-user-3.jpg",
+        positionInTheList: 3,
+        amountOfMoney: "25%",
+        id: 3,
+        name: "Marina",
+        surname: "Agapova",
+      },
+      158: {
+        avatar: "ava-user-158.jpg",
+        positionInTheList: 158,
+        amountOfMoney: "20,5%",
+        id: 158,
+        name: "My",
+        surname: "Position",
+      },
+    },
   }),
 };
 </script>
---
+++ src/components/operatorRating.vue
@@ -0,0 +1,276 @@
+<!--
+====== PROPS =====
+
+
+=== Object ===
+users = объект со списком рейтинга операторов
+
+Пример:
+users: {
+  1: {
+    avatar: undefined, - аватарка
+    positionInTheList: 1, - позиция в рейтинге
+    amountOfMoney: "$8,478.11", - значение
+    id: 1, - id оператора
+    name: "Sandro", - имя
+    surname: "Tavartkiladze", - фамилия
+  },
+  ...
+},
+=== /Object ===
+
+
+=== text ===
+title - заголовок карточки (значение String)
+=== /text ===
+
+
+=== background ===
+backgroundCard - фон всего компонента (значение классом Vuetify)
+backgroundTabs - фон для элемента v-tabs (значение классом Vuetify)
+backgroundAvatar - фон для элемента operator-rating-list__icon-box, который появляется если у объекта нет аватарки (значение классом Vuetify)
+=== /background ===
+
+
+=== border ===
+borderListItem = цвет для border-bottom у элемента v-list-item (значение HEX, RGB, RGBA, HSLA)
+=== border ===
+
+
+=== color text ===
+BaseColor - основной цвета для заголовка компонента, нумерации, имени и фамилии оператора (значение классом Vuetify)
+colorTabActive - цвет для активного элемента v-tab (значение HEX, RGB, RGBA, HSLA)
+colorAvatarText - цвет для аватарки, которая появляется если у объекта нет аватарки (значение классом Vuetify)
+ValueRatingColor - цвет значения в рейтинга оператора (значение классом Vuetify)
+=== /color text ===
+
+
+=== ШАБЛОН ===
+
+<operatorRating
+  title=""
+  
+  backgroundCard=""
+  backgroundTabs=""
+  backgroundAvatar=""
+
+  borderListItem=""
+
+  BaseColor=""
+  colorTabActive=""
+  colorAvatarText=""
+  ValueRatingColor=""
+/>
+
+--->
+
+<template>
+  <v-col
+    class="operator-rating rounded-xl"
+    :class="[backgroundCard]"
+    :style="cssProps"
+  >
+    <h6
+      class="text-subtitle-1 font-weight-regular mb-2 mt-4 mx-4 operator-rating__title"
+      :class="BaseColor"
+    >
+      {{ title }}
+    </h6>
+    <v-tabs
+      class="rounded-xl align-center mb-3"
+      :class="[backgroundTabs]"
+      active-class="tab-item_active"
+      slider-color="transparent"
+      :height="41"
+      :centered="true"
+    >
+      <v-tab
+        v-for="(value, key) in tabs"
+        :height="20"
+        :key="key"
+        class="rounded-xl white--text text-body-2 font-weight-medium text-capitalize ma-1 flex-grow-1"
+        >{{ value }}</v-tab
+      >
+    </v-tabs>
+    <v-list one-line color="transparent" class="operator-rating-list pa-0">
+      <template v-for="user in users">
+        <v-list-item :key="user.id" class="operator-rating-list__item">
+          <span
+            class="operator-rating-list__number text-center px-1"
+            :class="BaseColor"
+            >{{ user.positionInTheList }}</span
+          >
+          <v-list-item-avatar
+            :color="backgroundAvatar"
+            height="50"
+            width="50"
+            class="operator-rating-list__avatar ma-0"
+          >
+            <v-img
+              :src="require(`../assets/images/operatorRating/${user.avatar}`)"
+              alt="avatar"
+              v-if="user.avatar"
+            />
+            <div
+              v-else
+              class="avatar-box text-body-2 transparent--text"
+              :class="colorAvatarText"
+            >
+              {{ user.name[0] }}{{ user.surname[0] }}
+            </div>
+          </v-list-item-avatar>
+          <span
+            class="d-inline-block ml-4 font-weight-medium body-2"
+            :class="BaseColor"
+            >{{ user.name }} {{ user.surname }}</span
+          >
+          <span
+            class="d-inline-block body-1 ml-auto"
+            :class="ValueRatingColor"
+            >{{ user.amountOfMoney }}</span
+          >
+          <template>
+            <v-list-item-icon v-if="user.positionInTheList < 4">
+              <v-img
+                height="20"
+                width="17"
+                class="mr-1"
+                :src="
+                  require(`../assets/images/operatorRating/icon-top-${user.positionInTheList}.svg`)
+                "
+            /></v-list-item-icon>
+            <div v-else class="operator-rating-list__icon-box ml-4"></div>
+          </template>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "operatorRating",
+  data: () => ({
+    tabs: {
+      el_1: "Прошлый",
+      el_2: "Текущий месяц",
+      el_3: "Сегодня",
+    },
+  }),
+
+  props: {
+    // text
+    title: {
+      type: String,
+      default: "Топ операторов",
+    },
+
+    // color
+    BaseColor: {
+      type: String,
+      default: "white--text",
+    },
+    colorTabActive: {
+      type: String,
+      default: "#70ae9d",
+    },
+
+    // border
+    borderListItem: {
+      type: String,
+      default: "#70ae9d",
+    },
+
+    colorAvatarText: {
+      type: String,
+      default: "teal--text",
+    },
+    ValueRatingColor: {
+      type: String,
+      default: "lime--text text--accent-2",
+    },
+
+    // background
+    backgroundCard: {
+      type: String,
+      default: "teal lighten-3",
+    },
+    backgroundTabs: {
+      type: String,
+      default: "teal lighten-2",
+    },
+
+    backgroundAvatar: {
+      type: String,
+      default: "teal lighten-5",
+    },
+
+    // object users
+    users: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+
+  computed: {
+    cssProps() {
+      return {
+        "--color-tab-active": this.colorTabActive,
+        "--color-list-item-border": this.borderListItem,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.operator-rating {
+  //=== vars ===
+  --color-tab-active: #70ae9d;
+  --color-list-item-border: rgba(31, 120, 96, 0.4);
+  // ===/ vars ===
+
+  max-width: 385px !important;
+  width: 100% !important;
+
+  &__title {
+    letter-spacing: 0.07rem !important;
+  }
+
+  // === tabs ===
+  .v-tabs-bar {
+    background-color: transparent !important;
+  }
+
+  .tab-item_active {
+    background: #fff;
+    color: var(--color-tab-active) !important;
+  }
+  // === /tabs ===
+
+  // === list ===
+  &-list {
+    &__item {
+      padding: 9px 0 !important;
+
+      &:not(:last-child) {
+        border-bottom: 1px solid var(--color-list-item-border);
+      }
+    }
+
+    &__avatar {
+      border-radius: 15px !important;
+    }
+
+    &__number {
+      width: 39px;
+    }
+
+    &__icon-box {
+      width: 22px !important;
+      height: 20px !important;
+    }
+  }
+  // === /list ===
+}
+</style>
